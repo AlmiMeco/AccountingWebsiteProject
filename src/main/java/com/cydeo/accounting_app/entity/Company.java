e5b6c407ddf5559f0fc597135fddc041d78d39d0
denispupkov <denis.pupkov@gmail.com>
2023-06-26 17:01:31 -0600

    Company Entity created

---
+++ src/main/java/com/cydeo/accounting_app/entity/Company.java
@@ -0,0 +1,35 @@
+package com.cydeo.accounting_app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "companies")
+@Where(clause = "is_deleted = false")
+public class Company extends BaseEntity {
+
+    @Column(unique = true)
+    private String title;
+
+    private String phone;
+    private String website;
+
+    @Enumerated(EnumType.STRING)
+    private CompanyStatus companyStatus;
+
+    @OneToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+
+
+}
