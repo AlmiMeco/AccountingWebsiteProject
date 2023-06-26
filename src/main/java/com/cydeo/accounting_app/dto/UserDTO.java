28257a5e1ef434f1c3adbe233604cfe7c730493a
AlmiMeco <almimeci@gmail.com>
2023-06-26 15:29:03 -0500

    UserDTO created

---
+++ src/main/java/com/cydeo/accounting_app/dto/UserDTO.java
@@ -0,0 +1,22 @@
+package com.cydeo.accounting_app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Long id;
+
+    private String userName, password, confirmPassword, firstName, lastName, phone;
+
+//    private RoleDTO role;
+
+//    private CompanyDTO company;
+
+    private boolean isOnlyAdmin;
+
+}
