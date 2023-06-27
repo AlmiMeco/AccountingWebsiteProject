cabe3fad0a9d2191cd0730a42f9487531d0ac365
Bob <rikhsiboev@gmail.com>
2023-06-27 10:19:40 -0600

    fixed

--- src/main/java/com/cydeo/accounting_app/entity/ClientVendor.java
+++ src/main/java/com/cydeo/accounting_app/entity/ClientVendor.java
@@ -18,7 +18,7 @@ public class ClientVendor extends BaseEntity {
     private String phone;
     private String website;
     @Enumerated(EnumType.STRING)
-    private ClientVendorType clientVendorTypeEnum;
+    private ClientVendorType clientVendorType;
     @OneToOne
     @Column(name = "address_id")
     @Cascade({org.hibernate.annotations.CascadeType.MERGE, org.hibernate.annotations.CascadeType.PERSIST})
