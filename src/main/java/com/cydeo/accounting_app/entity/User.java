00cff7695005dddc352b4b949669460d237935ff
AlmiMeco <almimeci@gmail.com>
2023-06-27 11:13:13 -0500

    UserDTO - > Company field uncommented && LAZY fetch type added

--- src/main/java/com/cydeo/accounting_app/entity/User.java
+++ src/main/java/com/cydeo/accounting_app/entity/User.java
@@ -3,10 +3,7 @@ package com.cydeo.accounting_app.entity;
 import lombok.*;
 import org.hibernate.annotations.Where;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -30,10 +27,10 @@ public class User extends BaseEntity {
 
     private boolean enabled;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Role role;
 
-//    @ManyToOne
-//    private Company company;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Company company;
 
 }
