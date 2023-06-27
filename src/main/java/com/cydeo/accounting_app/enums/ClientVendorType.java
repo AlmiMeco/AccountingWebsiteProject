87b9a89b8b85472d74bb309cf471c1b9265a6cad
Bob <rikhsiboev@gmail.com>
2023-06-26 18:50:10 -0600

    VENDOR("Vendor"), CLIENT("Client");

--- src/main/java/com/cydeo/accounting_app/enums/ClientVendorType.java
+++ src/main/java/com/cydeo/accounting_app/enums/ClientVendorType.java
@@ -1,7 +1,7 @@
 package com.cydeo.accounting_app.enums;
 
 public enum ClientVendorType {
-    VENDOR_TYPE("Vendor"), CLIENT_VENDOR_TYPE("Client");
+    VENDOR("Vendor"), CLIENT("Client");
     private final String value;
 
     ClientVendorType(String value) {
