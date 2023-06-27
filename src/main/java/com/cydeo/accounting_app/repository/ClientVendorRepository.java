f72476f6c8fdfa7238493e69ff4563b53233380f
Bob <rikhsiboev@gmail.com>
2023-06-27 15:38:50 -0600

    MAVEN-20 Create "ClientVendorRepository" interface

---
+++ src/main/java/com/cydeo/accounting_app/repository/ClientVendorRepository.java
@@ -0,0 +1,13 @@
+package com.cydeo.accounting_app.repository;
+
+import com.cydeo.accounting_app.entity.ClientVendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+@Repository
+public interface ClientVendorRepository extends JpaRepository<ClientVendor,Long> {
+    List<ClientVendor> findAll();
+    Optional<ClientVendor> delete(Long id);
+}
