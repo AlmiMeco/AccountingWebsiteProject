880286a4a4db1a2968f8d75147722c6790b8b9a2
AlmiMeco <almimeci@gmail.com>
2023-06-27 14:12:17 -0500

    RoleRepository, RoleService, RoleServiceImpl Created

---
+++ src/main/java/com/cydeo/accounting_app/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.cydeo.accounting_app.repository;
+
+import com.cydeo.accounting_app.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+
+
+}
---
+++ src/main/java/com/cydeo/accounting_app/service/implementation/RoleServiceImpl.java
@@ -0,0 +1,6 @@
+package com.cydeo.accounting_app.service.implementation;
+
+import com.cydeo.accounting_app.service.RoleService;
+
+public class RoleServiceImpl implements RoleService {
+}
