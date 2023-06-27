17be5f33dd33a8a66e683040c8ed223888de9d02
AlmiMeco <almimeci@gmail.com>
2023-06-27 14:37:28 -0500

    RoleServiceImpl -> findById() method implemented

--- src/main/java/com/cydeo/accounting_app/repository/RoleRepository.java
+++ src/main/java/com/cydeo/accounting_app/repository/RoleRepository.java
@@ -3,8 +3,10 @@ package com.cydeo.accounting_app.repository;
 import com.cydeo.accounting_app.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-
+    Optional<Role> findById(Long id);
 
 }
--- src/main/java/com/cydeo/accounting_app/service/RoleService.java
+++ src/main/java/com/cydeo/accounting_app/service/RoleService.java
@@ -1,4 +1,13 @@
 package com.cydeo.accounting_app.service;
 
+import com.cydeo.accounting_app.dto.RoleDTO;
+
+import java.util.List;
+
 public interface RoleService {
+
+    RoleDTO findById(Long id);
+
+    List<RoleDTO> listAllRoles();
+
 }
