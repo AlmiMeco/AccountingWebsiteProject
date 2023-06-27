696661a7b8d8761ce4a348f4d5b13366ecb9a292
AlmiMeco <almimeci@gmail.com>
2023-06-27 17:31:23 -0500

    UserController :
    { End-user should be able to List (display) all Users in the user_list page...
      End-user should be able to Edit each User, when click on Edit button, end-user should land on user_update page and the edit form should be populated with the information of that very same User.
      End-user should be able to Delete each User (soft delete), then end up to the user_list page with updated User list.
      When End-User clicks on "Create-User" button, user_create page should be displayed with an Empty user form, }

---
+++ src/main/java/com/cydeo/accounting_app/controllers/UserController.java
@@ -0,0 +1,49 @@
+package com.cydeo.accounting_app.controllers;
+
+import com.cydeo.accounting_app.dto.UserDTO;
+import com.cydeo.accounting_app.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("/create")
+    public String userCreate(Model model){
+
+        model.addAttribute("user", new UserDTO());
+
+        return "/user/user-create";
+    }
+
+    @GetMapping("/list")
+    public String listUser(Model model){
+        //        Not Working (requires UserService impl -> listAllUsers() method)
+        model.addAttribute("users", userService.listAllUsers() );
+
+        return "/user/user-list";
+
+    }
+
+    @GetMapping("/list/{id}")
+    public String editUser(@PathVariable("id") Long id, Model model){
+        //        Not Working (requires UserService impl -> findById() method)
+        model.addAttribute("users", userService.findById(id));
+
+        return "/user/user-update";
+
+    }
+
+
+}
