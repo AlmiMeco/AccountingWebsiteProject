48f4281fcb823ca57a40ae2b1ea0464dfd5d6500
AlmiMeco <almimeci@gmail.com>
2023-06-27 16:46:52 -0500

    UserDTOConverter created

---
+++ src/main/java/com/cydeo/accounting_app/converter/UserDTOConverter.java
@@ -0,0 +1,20 @@
+package com.cydeo.accounting_app.converter;
+
+import com.cydeo.accounting_app.dto.UserDTO;
+import com.cydeo.accounting_app.service.UserService;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.convert.converter.Converter;
+
+public class UserDTOConverter implements Converter<String , UserDTO> {
+
+    private final UserService userService;
+
+    public UserDTOConverter(@Lazy UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDTO convert(String source) {
+        return userService.findByUsername(source);
+    }
+}
