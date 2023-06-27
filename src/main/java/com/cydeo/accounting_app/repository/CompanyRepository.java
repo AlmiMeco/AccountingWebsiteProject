db001a1e75a92649322d9bf6a0130731b61f8895
denispupkov <denis.pupkov@gmail.com>
2023-06-27 17:40:49 -0600

    CompanyRepository + CompanyService + CompanyServiceImpl have been added

---
+++ src/main/java/com/cydeo/accounting_app/repository/CompanyRepository.java
@@ -0,0 +1,17 @@
+package com.cydeo.accounting_app.repository;
+
+import com.cydeo.accounting_app.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+
+    Optional<Company> findCompanyById(Long id);
+    List<Company> listAllCompanies();
+
+
+}
--- src/main/java/com/cydeo/accounting_app/service/implementation/CompanyServiceImpl.java
+++ src/main/java/com/cydeo/accounting_app/service/implementation/CompanyServiceImpl.java
@@ -1,19 +1,40 @@
 package com.cydeo.accounting_app.service.implementation;
 
 import com.cydeo.accounting_app.dto.CompanyDTO;
+import com.cydeo.accounting_app.entity.Company;
+import com.cydeo.accounting_app.mapper.MapperUtil;
+import com.cydeo.accounting_app.repository.CompanyRepository;
 import com.cydeo.accounting_app.service.CompanyService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
 @Service
 public class CompanyServiceImpl implements CompanyService {
+
+    private final CompanyRepository companyRepository;
+    private final MapperUtil mapper;
+
+    public CompanyServiceImpl(CompanyRepository companyRepository, MapperUtil mapper) {
+        this.companyRepository = companyRepository;
+        this.mapper = mapper;
+    }
+
     @Override
     public List<CompanyDTO> listAllCompanies() {
-        return null;
+         return companyRepository.listAllCompanies().stream().
+                 map(company -> mapper.convert(company, new CompanyDTO())).
+                 collect(Collectors.toList());
+
     }
 
     @Override
     public CompanyDTO findById(Long id) {
-        return null;
+        Company company = companyRepository.findCompanyById(id).
+                orElseThrow(() -> new NoSuchElementException
+                        ("Company with id# "+id+" is not found"));
+        return mapper.convert(company, new CompanyDTO());
     }
 }
