65da210a38b732ccbaed0ff276be6fa09e87a5c8
Hasanain_Cydeo <hasanainalameen@gmail.com>
2023-06-26 21:57:12 -0500

    MAVEN-22 - Create CategoryServiceImpl class

---
+++ src/main/java/com/cydeo/accounting_app/service/implementation/CategoryServiceImpl.java
@@ -0,0 +1,10 @@
+package com.cydeo.accounting_app.service.implementation;
+
+import com.cydeo.accounting_app.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+
+}
