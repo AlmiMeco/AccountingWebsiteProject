63cfcb5c24af8d6bb3a8cae2393f7e6eec549f46
Bob <rikhsiboev@gmail.com>
2023-06-28 09:00:15 -0600

    MAVEN-20 ClientVendorRepository issue fix

--- src/main/java/com/cydeo/accounting_app/repository/ClientVendorRepository.java
+++ src/main/java/com/cydeo/accounting_app/repository/ClientVendorRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 import java.util.Optional;
 @Repository
 public interface ClientVendorRepository extends JpaRepository<ClientVendor,Long> {
-    List<ClientVendor> findAll();
+
 }
