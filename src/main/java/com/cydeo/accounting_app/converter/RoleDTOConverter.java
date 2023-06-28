409d810077cc5df62e540b6921d1f4cdff6ebce8
AlmiMeco <almimeci@gmail.com>
2023-06-28 00:41:46 -0500

    ADDED:
    RoleDTOConverter -> Null source handled

--- src/main/java/com/cydeo/accounting_app/converter/RoleDTOConverter.java
+++ src/main/java/com/cydeo/accounting_app/converter/RoleDTOConverter.java
@@ -19,6 +19,9 @@ public class RoleDTOConverter implements Converter<String, RoleDTO> {
 
     @Override
     public RoleDTO convert(String source) {
+
+        if (source == null || source.equals("")) {return null;}
+
         return roleService.findById(Long.parseLong(source));
     }
 
