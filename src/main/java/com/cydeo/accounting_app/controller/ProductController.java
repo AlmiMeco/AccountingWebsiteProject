6e8a83f6a99107081f2e5d4bca828b7d441b0eef
Hasanain_Cydeo <hasanainalameen@gmail.com>
2023-06-29 07:26:57 -0500

    MAVEN-23 - create ProductController class.

---
+++ src/main/java/com/cydeo/accounting_app/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.cydeo.accounting_app.controller;
+
+import com.cydeo.accounting_app.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/list")
+    public String displayAllProducts(Model model) {
+        model.addAttribute("products", productService.getProductList());
+        return "/product/product-list";
+    }
+
+}
