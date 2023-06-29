4a1ad75b39b2a2d6d4054ab24f4a9b8155eec2e7
denispupkov <denis.pupkov@gmail.com>
2023-06-28 23:53:59 -0600

    import of unused Service removed

--- src/main/java/com/cydeo/accounting_app/controller/CompanyController.java
+++ src/main/java/com/cydeo/accounting_app/controller/CompanyController.java
@@ -4,7 +4,6 @@ import com.cydeo.accounting_app.dto.CompanyDTO;
 import com.cydeo.accounting_app.service.CompanyService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
