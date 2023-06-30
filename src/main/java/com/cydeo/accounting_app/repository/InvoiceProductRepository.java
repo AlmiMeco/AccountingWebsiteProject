837cb695d06cfb994814835a0f85e9a1f5987790
Kemail-terminal <sulkemref@gmail.com>
2023-06-29 21:15:51 -0500

    MAVEN-24 - InvoiceProductRepository done

--- src/main/java/com/cydeo/accounting_app/repository/InvoiceProductRepository.java
+++ src/main/java/com/cydeo/accounting_app/repository/InvoiceProductRepository.java
@@ -1,4 +1,15 @@
 package com.cydeo.accounting_app.repository;
 
-public interface InvoiceProductRepository {
+import com.cydeo.accounting_app.entity.Invoice;
+import com.cydeo.accounting_app.entity.InvoiceProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InvoiceProductRepository extends JpaRepository<InvoiceProduct,Long> {
+
+    List<InvoiceProduct> findAllByInvoice(Invoice invoice);
 }
+
