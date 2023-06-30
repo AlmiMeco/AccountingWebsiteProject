f18fcd7f35c21373b0bab646a2fe0dd1cdc87479
Kemail-terminal <sulkemref@gmail.com>
2023-06-29 21:14:50 -0500

    MAVEN-24 - InvoiceService and InvoiceServiceImpl done

--- src/main/java/com/cydeo/accounting_app/service/implementation/InvoiceServiceImpl.java
+++ src/main/java/com/cydeo/accounting_app/service/implementation/InvoiceServiceImpl.java
@@ -1,4 +1,93 @@
 package com.cydeo.accounting_app.service.implementation;
 
-public class InvoiceServiceImpl {
+import com.cydeo.accounting_app.dto.InvoiceDTO;
+import com.cydeo.accounting_app.dto.ProductDTO;
+import com.cydeo.accounting_app.entity.Invoice;
+import com.cydeo.accounting_app.enums.InvoiceStatus;
+import com.cydeo.accounting_app.enums.InvoiceType;
+import com.cydeo.accounting_app.mapper.MapperUtil;
+import com.cydeo.accounting_app.repository.InvoiceRepository;
+import com.cydeo.accounting_app.service.InvoiceService;
+import com.cydeo.accounting_app.service.LoggedInUserService;
+import com.cydeo.accounting_app.service.SecurityService;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class InvoiceServiceImpl extends LoggedInUserService implements InvoiceService {
+
+    private final InvoiceRepository invoiceRepository;
+
+    public InvoiceServiceImpl(SecurityService securityService, MapperUtil mapperUtil, InvoiceRepository invoiceRepository) {
+        super(securityService, mapperUtil);
+        this.invoiceRepository = invoiceRepository;
+    }
+
+    @Override
+    public InvoiceDTO findById(Long id) {
+        Invoice invoice = invoiceRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("This Invoice does not exist"));
+        if(invoice.isDeleted){
+            throw new RuntimeException("The invoice has been deleted");
+        }
+        return mapperUtil.convert(invoice,new InvoiceDTO());
+    }
+
+    @Override
+    public List<InvoiceDTO> listAllInvoicesByType(InvoiceType type) {
+        return invoiceRepository.findAllByInvoiceType(type).stream()
+                .map(invoice -> mapperUtil.convert(invoice,new InvoiceDTO()))
+                .collect(Collectors.toList());
+    }
+    @Override
+    public void saveInvoiceByType(InvoiceDTO invoiceDTO,InvoiceType type) {
+        Invoice invoice = mapperUtil.convert(invoiceDTO, new Invoice());
+        invoice.setInvoiceType(type);
+        invoice.setInvoiceStatus(InvoiceStatus.AWAITING_APPROVAL);
+        invoiceRepository.save(invoice);
+    }
+
+    @Override
+    public void deleteInvoiceById(Long id) {
+        Invoice invoice = invoiceRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Invoice does not exist"));
+        invoice.setIsDeleted(true);
+        invoiceRepository.save(invoice);
+    }
+
+    @Override
+    public void addNewProductToInvoice(Long invoiceId, ProductDTO productDTO) {
+
+    }
+
+    @Override
+    public void approveInvoiceById(Long id) {
+        Invoice invoice = invoiceRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Invoice does not exist"));
+        invoice.setInvoiceStatus(InvoiceStatus.APPROVED);
+        invoiceRepository.save(invoice);
+    }
+
+    @Override
+    public InvoiceDTO createInvoice(InvoiceType type) {
+        InvoiceDTO invoiceDTO = new InvoiceDTO();
+        int value = invoiceRepository.findAll().stream()
+                .filter(invoice -> invoice.getInvoiceType()==type)
+                .map(invoice -> Integer.parseInt(invoice.getInvoiceNo().substring(2)))
+                .max(Integer::compareTo).orElse(0);
+        value++;
+        String InvoiceNo = type.getValue().charAt(0)+
+                "-"+((value<100)?"0":"") +((value<10)?"0":"")+value;
+        invoiceDTO.setInvoiceNo(InvoiceNo);
+        invoiceDTO.setDate(LocalDate.now());
+        return invoiceDTO;
+    }
+
+    @Override
+    public String findLastInvoiceId() {
+        return String.valueOf(invoiceRepository.findMaxNumberInvoiceId());
+    }
 }
