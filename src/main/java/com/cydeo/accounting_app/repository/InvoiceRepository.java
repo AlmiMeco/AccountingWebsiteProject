20cda66419c8aaa4d6a71aa5b6a43e0d8019213f
Kemail-terminal <sulkemref@gmail.com>
2023-06-29 21:15:00 -0500

    MAVEN-24 - InvoiceRepository done

--- src/main/java/com/cydeo/accounting_app/repository/InvoiceRepository.java
+++ src/main/java/com/cydeo/accounting_app/repository/InvoiceRepository.java
@@ -3,14 +3,16 @@ package com.cydeo.accounting_app.repository;
 import com.cydeo.accounting_app.entity.Invoice;
 import com.cydeo.accounting_app.enums.InvoiceType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice,Long> {
-
     List<Invoice> findAllByInvoiceType(InvoiceType invoiceType);
 
+    @Query(value = "SELECT MAX(id) FROM invoices",nativeQuery = true)
+    long findMaxNumberInvoiceId();
 
 }
