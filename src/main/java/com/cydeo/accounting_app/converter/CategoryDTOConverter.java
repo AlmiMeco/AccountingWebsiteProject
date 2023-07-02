c08714b91efc08fab32ae82b47addafcfc4774a3
Kemail-terminal <sulkemref@gmail.com>
2023-07-02 17:40:13 -0500

    MAVEN-63 Dto converters fixed

--- src/main/java/com/cydeo/accounting_app/converter/CategoryDTOConverter.java
+++ src/main/java/com/cydeo/accounting_app/converter/CategoryDTOConverter.java
@@ -2,23 +2,24 @@ package com.cydeo.accounting_app.converter;
 
 import com.cydeo.accounting_app.dto.CategoryDTO;
 import com.cydeo.accounting_app.service.CategoryService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConfigurationPropertiesBinding
 public class CategoryDTOConverter implements Converter<String, CategoryDTO> {
 
-    CategoryService categoryService;
+    private final CategoryService categoryService;
 
-    public CategoryDTOConverter(CategoryService categoryService) {
+    public CategoryDTOConverter(@Lazy CategoryService categoryService) {
         this.categoryService = categoryService;
     }
 
     @Override
     public CategoryDTO convert(String source) {
-        if (source == null || source.isBlank()) {
-            return null;
-        }
+        if (source == null || source.isBlank()) {return null;}
         return categoryService.findById(Long.valueOf(source));
     }
 }
--- src/main/java/com/cydeo/accounting_app/converter/ClientVendorDTOConverter.java
+++ src/main/java/com/cydeo/accounting_app/converter/ClientVendorDTOConverter.java
@@ -2,17 +2,23 @@ package com.cydeo.accounting_app.converter;
 
 import com.cydeo.accounting_app.dto.ClientVendorDTO;
 import com.cydeo.accounting_app.service.ClientVendorService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConfigurationPropertiesBinding
 public class ClientVendorDTOConverter implements Converter<String, ClientVendorDTO> {
-    ClientVendorService clientVendorDTO;
-    public ClientVendorDTOConverter(ClientVendorService clientVendorDTO) {
+    private final ClientVendorService clientVendorDTO;
+
+    public ClientVendorDTOConverter(@Lazy ClientVendorService clientVendorDTO) {
         this.clientVendorDTO = clientVendorDTO;
     }
+
     @Override
     public ClientVendorDTO convert(String source) {
+        if (source == null || source.equals("")) {return null;}
         return clientVendorDTO.findById(Long.valueOf(source));
     }
 
