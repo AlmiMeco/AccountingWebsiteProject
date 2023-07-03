236459cab20be3b8cd259c065ac03b1f27870179
AlmiMeco <almimeci@gmail.com>
2023-07-02 20:36:10 -0500

    ADDED:
    ReportingController, ReportingService, and ReportingServiceImpl

---
+++ src/main/java/com/cydeo/accounting_app/service/implementation/ReportingServiceImpl.java
@@ -0,0 +1,8 @@
+package com.cydeo.accounting_app.service.implementation;
+
+import com.cydeo.accounting_app.service.ReportingService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportingServiceImpl implements ReportingService {
+}
