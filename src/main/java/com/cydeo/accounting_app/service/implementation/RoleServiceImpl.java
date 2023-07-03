9f1f47bf2830adb7143a6c6d67cde4bed9933006
AlmiMeco <almimeci@gmail.com>
2023-07-02 19:59:43 -0500

    ADDED:
    RoleServiceImpl -> listAllRoles() -> implemented w/ LoggedUser Service

--- src/main/java/com/cydeo/accounting_app/service/implementation/RoleServiceImpl.java
+++ src/main/java/com/cydeo/accounting_app/service/implementation/RoleServiceImpl.java
@@ -1,49 +1,63 @@
 package com.cydeo.accounting_app.service.implementation;
 
 import com.cydeo.accounting_app.dto.RoleDTO;
+import com.cydeo.accounting_app.dto.UserDTO;
 import com.cydeo.accounting_app.entity.Role;
+import com.cydeo.accounting_app.entity.User;
 import com.cydeo.accounting_app.mapper.MapperUtil;
 import com.cydeo.accounting_app.repository.RoleRepository;
+import com.cydeo.accounting_app.repository.UserRepository;
+import com.cydeo.accounting_app.service.LoggedInUserService;
 import com.cydeo.accounting_app.service.RoleService;
+import com.cydeo.accounting_app.service.SecurityService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 import org.springframework.stereotype.Service;
 
-
-import java.util.stream.Collectors;
-
 @Service
-public class RoleServiceImpl implements RoleService {
+public class RoleServiceImpl extends LoggedInUserService implements RoleService {
 
-    private final RoleRepository repository;
-    private final MapperUtil mapper;
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
 
-    public RoleServiceImpl(RoleRepository repository, MapperUtil mapper) {
-        this.repository = repository;
-        this.mapper = mapper;
+    public RoleServiceImpl(SecurityService securityService, MapperUtil mapperUtil, RoleRepository roleRepository, UserRepository userRepository) {
+        super(securityService, mapperUtil);
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
     }
+
 
     @Override
     public RoleDTO findById(Long id) {
 
-        Role role = repository.findById(id).orElseThrow(
+        Role role = roleRepository.findById(id).orElseThrow(
                 () -> new UsernameNotFoundException("Role with given ID does not exist")
         );
 
-        return mapper.convert(role, new RoleDTO());
+        return mapperUtil.convert(role, new RoleDTO());
 
     }
 
     @Override
     public List<RoleDTO> listAllRoles() {
 
+        User currentLoggedInUser = userRepository.getUserById(getCurrentUser().id);
 
-        return repository.findAll().stream()
-                .map(i -> mapper.convert(i, new RoleDTO()))
+        if (currentLoggedInUser.getId() == 1L) {
+
+            return roleRepository.findById(2L).stream()
+                    .map(i -> mapperUtil.convert(i, new RoleDTO()))
+                    .collect(Collectors.toList());
+
+        }else return roleRepository.findAll()
+                .stream()
+                .filter(i -> i.getId() >= 2L)
+                .map(i -> mapperUtil.convert(i, new RoleDTO()))
                 .collect(Collectors.toList());
 
     }
