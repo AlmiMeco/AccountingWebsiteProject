c3d605d1f85a69f9a3183ec6c6a442786713f76c
denispupkov <denis.pupkov@gmail.com>
2023-07-06 01:06:09 -0600

    made ProfitLoss report more dynamic changing ReportingServiceImpl and adding new script to InvoiceProductRepository, but still requires more improvements

--- src/main/java/com/cydeo/accounting_app/repository/InvoiceProductRepository.java
+++ src/main/java/com/cydeo/accounting_app/repository/InvoiceProductRepository.java
@@ -1,11 +1,17 @@
 package com.cydeo.accounting_app.repository;
 
+import com.cydeo.accounting_app.entity.Company;
 import com.cydeo.accounting_app.entity.Invoice;
 import com.cydeo.accounting_app.entity.InvoiceProduct;
 import com.cydeo.accounting_app.enums.InvoiceStatus;
+import com.cydeo.accounting_app.enums.InvoiceType;
+import com.cydeo.accounting_app.service.SecurityService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -23,6 +29,11 @@ public interface InvoiceProductRepository extends JpaRepository<InvoiceProduct,L
             "FROM InvoiceProduct invPr JOIN Invoice i on i.id = invPr.id " +
             "WHERE i.invoiceStatus = ?1")
     List<InvoiceProduct> findAllInvoiceProductsByInvoiceStatus(InvoiceStatus invoiceStatus);
+
+    @Query("SELECT invPr.profitLoss " +
+            "FROM InvoiceProduct invPr JOIN Invoice i on i.id = invPr.id " +
+            "WHERE i.company.id = ?1 AND i.invoiceStatus = ?2 AND invPr.invoice.invoiceType = ?3 AND i.isDeleted = ?4")
+    BigDecimal findInvoiceProductProfitLossByCompanyIdByInvoiceStatusByInvoiceTypeAndIsDeleted(Long companyId, InvoiceStatus invoiceStatus, InvoiceType invoiceType, boolean isDeleted);
 
 
 }
--- src/main/java/com/cydeo/accounting_app/service/implementation/ReportingServiceImpl.java
+++ src/main/java/com/cydeo/accounting_app/service/implementation/ReportingServiceImpl.java
@@ -1,9 +1,13 @@
 package com.cydeo.accounting_app.service.implementation;
 
 import com.cydeo.accounting_app.dto.InvoiceDTO;
+import com.cydeo.accounting_app.enums.InvoiceStatus;
 import com.cydeo.accounting_app.enums.InvoiceType;
+import com.cydeo.accounting_app.repository.InvoiceProductRepository;
+import com.cydeo.accounting_app.service.CompanyService;
 import com.cydeo.accounting_app.service.InvoiceService;
 import com.cydeo.accounting_app.service.ReportingService;
+import com.cydeo.accounting_app.service.SecurityService;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -16,9 +20,15 @@ public class ReportingServiceImpl implements ReportingService {
 
 
     private final InvoiceService invoiceService;
+    private final InvoiceProductRepository invoiceProductRepository;
+    private final CompanyService companyService;
+    private final SecurityService securityService;
 
-    public ReportingServiceImpl(InvoiceService invoiceService) {
+    public ReportingServiceImpl(InvoiceService invoiceService, InvoiceProductRepository invoiceProductRepository, CompanyService companyService, SecurityService securityService) {
         this.invoiceService = invoiceService;
+        this.invoiceProductRepository = invoiceProductRepository;
+        this.companyService = companyService;
+        this.securityService = securityService;
     }
 
     @Override
@@ -26,18 +36,24 @@ public class ReportingServiceImpl implements ReportingService {
 
         Map<String, BigDecimal> map = new HashMap<>();
 
-        BigDecimal totalSales = invoiceService.listAllInvoicesByType(InvoiceType.SALES).stream()
-                .map(InvoiceDTO::getPrice)
-                .reduce(BigDecimal::add).orElseGet(() -> new BigDecimal(0));
+        //BigDecimal totalSales = invoiceService.listAllInvoicesByType(InvoiceType.SALES).stream()
+         //       .map(InvoiceDTO::getPrice)
+          //      .reduce(BigDecimal::add).orElseGet(() -> new BigDecimal(0));
+        var profitLoss =  invoiceProductRepository.findInvoiceProductProfitLossByCompanyIdByInvoiceStatusByInvoiceTypeAndIsDeleted
+                (securityService.getLoggedInUser().getCompany().getId(), InvoiceStatus.APPROVED, InvoiceType.SALES, false);
 
-        BigDecimal totalCost = invoiceService.listAllInvoicesByType(InvoiceType.PURCHASE).stream()
-                .map(InvoiceDTO::getPrice)
-                .reduce(BigDecimal::add).orElseGet(() -> new BigDecimal(0));
+        //BigDecimal totalCost = invoiceService.listAllInvoicesByType(InvoiceType.PURCHASE).stream()
+        //        .map(InvoiceDTO::getPrice)
+        //        .reduce(BigDecimal::add).orElseGet(() -> new BigDecimal(0));
 
-        var profitLoss = totalSales.subtract(totalCost);
+
+
+
+        //var profitLoss = totalSales.subtract(totalCost);
 
 //        Hard-Coded for now
-        map.put(LocalDate.now().getMonth().toString(), profitLoss);
+        map.put(LocalDate.now().getYear()+" "+LocalDate.now().minusMonths(3).getMonth().toString(), profitLoss);
+
         return map;
     }
 }
