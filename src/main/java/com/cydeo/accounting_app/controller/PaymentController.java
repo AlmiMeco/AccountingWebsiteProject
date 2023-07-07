08deabc9bb12f37aa25c5731dfa5aac7a7fb19d2
AlmiMeco <almimeci@gmail.com>
2023-07-07 14:25:06 -0500

    ADDED:
    PaymentController -> RequestMappingAdded

--- src/main/java/com/cydeo/accounting_app/controller/PaymentController.java
+++ src/main/java/com/cydeo/accounting_app/controller/PaymentController.java
@@ -1,7 +1,9 @@
 package com.cydeo.accounting_app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/payments")
 public class PaymentController {
 }
