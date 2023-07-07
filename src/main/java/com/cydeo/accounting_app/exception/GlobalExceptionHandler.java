91b4b028f384f0d11d4b66443d54ca94875aa0ea
Kemail-terminal <sulkemref@gmail.com>
2023-07-07 12:20:41 -0500

    MAVEN-80 GlobalExceptionHandler genericException and getMessageFromAnnotation

--- src/main/java/com/cydeo/accounting_app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cydeo/accounting_app/exception/GlobalExceptionHandler.java
@@ -1,4 +1,22 @@
 package com.cydeo.accounting_app.exception;
 
+import com.cydeo.accounting_app.annotation.DefaultExceptionMessage;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.HandlerMethod;
+
+import java.lang.reflect.Method;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler({Exception    .class, RuntimeException.class, Throwable.class})
+    public String genericException( HandlerMethod handlerMethod) {
+        return getMessageFromAnnotation(handlerMethod.getMethod());
+    }
+    private String getMessageFromAnnotation(Method method) {
+        DefaultExceptionMessage defaultExceptionMessage = method.getAnnotation(DefaultExceptionMessage.class);
+        return defaultExceptionMessage.defaultMessage();
+    }
+
 }
