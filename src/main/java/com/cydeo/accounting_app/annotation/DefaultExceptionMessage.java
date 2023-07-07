73f818ff91037bc14fc5f33c3798749975cbb105
Kemail-terminal <sulkemref@gmail.com>
2023-07-07 13:03:49 -0500

    MAVEN-80 DefaultExceptionMessage done

--- src/main/java/com/cydeo/accounting_app/annotation/DefaultExceptionMessage.java
+++ src/main/java/com/cydeo/accounting_app/annotation/DefaultExceptionMessage.java
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DefaultExceptionMessage {
 
-    String defaultMessage() default "Something went wrong!";
+    String defaultMessage() default "";
 
 }
--- src/main/java/com/cydeo/accounting_app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cydeo/accounting_app/exception/GlobalExceptionHandler.java
@@ -1,22 +1,43 @@
 package com.cydeo.accounting_app.exception;
 
 import com.cydeo.accounting_app.annotation.DefaultExceptionMessage;
+import com.cydeo.accounting_app.dto.DefaultExceptionMessageDTO;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.ui.Model;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.HandlerMethod;
 
 import java.lang.reflect.Method;
+import java.util.Optional;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({Exception    .class, RuntimeException.class, Throwable.class})
-    public String genericException( HandlerMethod handlerMethod) {
-        return getMessageFromAnnotation(handlerMethod.getMethod());
+    @ExceptionHandler({Exception    .class, RuntimeException.class, Throwable.class, BadCredentialsException.class})
+    public String genericException(Throwable e, HandlerMethod handlerMethod, Model model) {
+
+        Optional<DefaultExceptionMessageDTO> defaultMessage = getMessageFromAnnotation(handlerMethod.getMethod());
+        if (defaultMessage.isPresent() && !ObjectUtils.isEmpty(defaultMessage.get().getMessage())) {
+            model.addAttribute("message", defaultMessage.get().getMessage());
+            return "error";
+        }
+        model.addAttribute("message","Something went wrong!");
+        return "error";
     }
-    private String getMessageFromAnnotation(Method method) {
+
+
+    private Optional<DefaultExceptionMessageDTO> getMessageFromAnnotation(Method method) {
         DefaultExceptionMessage defaultExceptionMessage = method.getAnnotation(DefaultExceptionMessage.class);
-        return defaultExceptionMessage.defaultMessage();
+        if (defaultExceptionMessage != null) {
+            DefaultExceptionMessageDTO defaultExceptionMessageDTO = DefaultExceptionMessageDTO
+                    .builder()
+                    .message(defaultExceptionMessage.defaultMessage())
+                    .build();
+            return Optional.of(defaultExceptionMessageDTO);
+        }
+        return Optional.empty();
     }
 
 }
