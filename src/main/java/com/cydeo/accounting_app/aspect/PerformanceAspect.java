3736ca19e03205f463d4fb27fbafb07378f00fb1
Bob <rikhsiboev@gmail.com>
2023-07-07 15:31:51 -0600

    MAVEN-75 AOP & Logging Basic Functionality 2

--- src/main/java/com/cydeo/accounting_app/aspect/PerformanceAspect.java
+++ src/main/java/com/cydeo/accounting_app/aspect/PerformanceAspect.java
@@ -1,28 +1,30 @@
 package com.cydeo.accounting_app.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class PerformanceAspect {
-    private static final Logger LOGGER = LoggerFactory.getLogger(PerformanceAspect.class);
-
     @Around("@annotation(com.cydeo.accounting_app.annotation.ExecutionTime)")
-    public Object calculateExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
-        String methodName = joinPoint.getSignature().getName();
+    public Object calculateExecutionTime(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object object = null;
+        String methodName = proceedingJoinPoint.getSignature().getName();
+        log.info("Execution starts:");
         long startTime = System.currentTimeMillis();
-
         try {
-            return joinPoint.proceed();
-        } finally {
-            long endTime = System.currentTimeMillis();
-            long executionTime = endTime - startTime;
-            LOGGER.info("Method '{}' executed in {} milliseconds.", methodName, executionTime);
+            return proceedingJoinPoint.proceed();
+        } catch (Throwable throwable){
+           throwable.getStackTrace();
         }
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+        log.info("Time taken to execute: {} ms - Method: {}", executionTime, methodName);
+        return object;
     }
 }
