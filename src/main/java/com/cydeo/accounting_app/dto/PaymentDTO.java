03eccae997ffbffc06246be9b9f5cbad38957ade
AlmiMeco <almimeci@gmail.com>
2023-07-07 14:09:41 -0500

    ADDED:
    Payment entity
    PaymentDTO

---
+++ src/main/java/com/cydeo/accounting_app/dto/PaymentDTO.java
@@ -0,0 +1,27 @@
+package com.cydeo.accounting_app.dto;
+
+import com.cydeo.accounting_app.enums.Months;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentDTO {
+
+    private Long id;
+    private Integer year;
+    private Months month;
+    private LocalDate paymentDate;
+    private BigDecimal amount;
+    private Boolean isPaid;
+    private String companyStripeId;
+    private String description;
+    private CompanyDTO company;
+
+
+}
