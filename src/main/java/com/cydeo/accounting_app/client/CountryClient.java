c1f0993e36be9018762618dcd30654399c305244
Bob <rikhsiboev@gmail.com>
2023-07-08 17:38:18 -0600

    Maven 90 with Api

--- src/main/java/com/cydeo/accounting_app/client/CountryClient.java
+++ src/main/java/com/cydeo/accounting_app/client/CountryClient.java
@@ -1,17 +1,12 @@
 package com.cydeo.accounting_app.client;
 
+import com.cydeo.accounting_app.dto.CountryResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(url = "https://www.universal-tutorial.com/api", name = "COUNTRY-CLIENT")
 public interface CountryClient {
     @GetMapping("/countries")
-    List<String> getCountries();
-
+    CountryResponseDTO getCountries(@RequestHeader("Authorization") String authorization);
 }
-// uNAfy7_UQW5f1FvtdEzxepNiWs4Yiz-WKA3n_2qHXHj1T_ITEbZ8AOVaUd-dxloJDL4
-
-
-
--- src/main/java/com/cydeo/accounting_app/service/implementation/AddressServiceImpl.java
+++ src/main/java/com/cydeo/accounting_app/service/implementation/AddressServiceImpl.java
@@ -1,24 +1,30 @@
 package com.cydeo.accounting_app.service.implementation;
 
+import com.cydeo.accounting_app.client.CountryClient;
 import com.cydeo.accounting_app.repository.AddressRepository;
 import com.cydeo.accounting_app.service.AddressService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 
 @Service
 public class AddressServiceImpl implements AddressService {
+    @Value("${country.client.token}") String authorization;
 
     private final AddressRepository addressRepository;
+    private final CountryClient countryClient;
 
-    public AddressServiceImpl(AddressRepository addressRepository) {
+    public AddressServiceImpl(AddressRepository addressRepository, CountryClient countryClient) {
         this.addressRepository = addressRepository;
+        this.countryClient = countryClient;
     }
     @Override
     public List<String> listOfCountries() {
-        return List.of("United States");
+        return List.of(countryClient.getCountries(authorization).getCountryName());
     }
 
     @Override
