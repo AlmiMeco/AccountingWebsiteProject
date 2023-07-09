6ba38a74752de6aa80f9d381b3151511c9e228b9
AlmiMeco <almimeci@gmail.com>
2023-07-09 11:31:27 -0500

    ADDED:
    PaymentController -> [GET] payment list methods added

--- src/main/java/com/cydeo/accounting_app/controller/PaymentController.java
+++ src/main/java/com/cydeo/accounting_app/controller/PaymentController.java
@@ -1,9 +1,25 @@
 package com.cydeo.accounting_app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/payments")
 public class PaymentController {
+
+    @GetMapping("/list")
+    public String paymentsList(){
+
+
+
+        return "payment/payment-list";
+    }
+
+    @GetMapping("/list/{year}")
+    public String paymentListWithYearQuery(){
+
+
+        return "payment/payment-list";
+    }
 }
