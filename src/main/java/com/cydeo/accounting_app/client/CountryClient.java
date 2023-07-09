70e5127adf50a02fe3aab15746b80e02cc32473a
Bob <rikhsiboev@gmail.com>
2023-07-09 12:11:22 -0600

    Maven 90 with Api Done

--- src/main/java/com/cydeo/accounting_app/client/CountryClient.java
+++ src/main/java/com/cydeo/accounting_app/client/CountryClient.java
@@ -5,8 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
+import java.util.List;
+
 @FeignClient(url = "https://www.universal-tutorial.com/api", name = "COUNTRY-CLIENT")
 public interface CountryClient {
     @GetMapping("/countries")
-    CountryResponseDTO getCountries(@RequestHeader("Authorization") String authorization);
+    List<CountryResponseDTO> getCountries(@RequestHeader("Authorization") String authorization);
 }
--- src/main/java/com/cydeo/accounting_app/service/implementation/AddressServiceImpl.java
+++ src/main/java/com/cydeo/accounting_app/service/implementation/AddressServiceImpl.java
@@ -1,6 +1,7 @@
 package com.cydeo.accounting_app.service.implementation;
 
 import com.cydeo.accounting_app.client.CountryClient;
+import com.cydeo.accounting_app.dto.CountryResponseDTO;
 import com.cydeo.accounting_app.repository.AddressRepository;
 import com.cydeo.accounting_app.service.AddressService;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,11 +10,13 @@ import org.springframework.stereotype.Service;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 
 @Service
 public class AddressServiceImpl implements AddressService {
-    @Value("${country.client.token}") String authorization;
+    @Value("${country.client.token}")
+    String authorization;
 
     private final AddressRepository addressRepository;
     private final CountryClient countryClient;
@@ -24,7 +27,10 @@ public class AddressServiceImpl implements AddressService {
     }
     @Override
     public List<String> listOfCountries() {
-        return List.of(countryClient.getCountries(authorization).getCountryName());
+        List<String> countries = countryClient.getCountries(authorization).stream()
+                .map(CountryResponseDTO::getCountryName)
+                .collect(Collectors.toList());
+        return countries;
     }
 
     @Override
