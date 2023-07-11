a3ed5232b9db5d9a98be2175fe996605569266d7
Kemail-terminal <sulkemref@gmail.com>
2023-07-11 15:10:33 -0500

    MAVEN-80 GlobalExceptionHandler and All Exceptions unnecessary part removed.

--- src/main/java/com/cydeo/accounting_app/exception/InvoiceProductNotFoundException.java
+++ src/main/java/com/cydeo/accounting_app/exception/InvoiceProductNotFoundException.java
@@ -1,12 +1,8 @@
 package com.cydeo.accounting_app.exception;
 
 public class InvoiceProductNotFoundException extends RuntimeException{
-
     public InvoiceProductNotFoundException(String message) {
         super(message);
     }
 
-    public InvoiceProductNotFoundException(String message, Throwable err) {
-        super(message, err);
-    }
 }
